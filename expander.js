4c4410df183ea0b23074f3f98ac3c78dc5d6dc3c
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-19 15:34:01 -0400

    now toggle the background color of the square onclick

--- expander.js
+++ expander.js
@@ -11,10 +11,15 @@
             return squareElement.innerText == '+' ? '-' : '+';
 		}
 
+        function squareColor(squareElement) {
+            return squareElement.getStyle('background-color') == 'white' ? 'red' : 'white';
+        }
+            
         widgets.each(function(widget) {
 			var square = widget.squareElement;
             Event.observe(square, 'click', function() {
-				square.innerText = squareText(square);				
+				square.innerText = squareText(square);	
+				square.setStyle({ backgroundColor: squareColor(square)});				
                 Effect.toggle(widget.expanderElement, 'blind', { duration: 0.5 });
             });
         });
