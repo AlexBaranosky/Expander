4e2cba574baf0e4642dc5fb5a7ccf7b98cc07e6c
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-19 14:46:56 -0400

    replaced class with a simple associative array

--- expander.js
+++ expander.js
@@ -1,14 +1,10 @@
 (function() {
-	var ExpanderWidget = function(squareElement, expanderElement) {
-		this.squareElement = squareElement;
-		this.expanderElement = expanderElement;
-	};
-
     function addEvents() {	
 		var widgets = $$('div.section').collect(function(section) {
 			var square = Element.down(section, 'div.square');
 			var expander = Element.down(section, 'div.expander');
-			return new ExpanderWidget(square, expander);
+			return { squareElement: square, 
+			         expanderElement: expander};
 		})
 	
         widgets.each(function(widget) {
