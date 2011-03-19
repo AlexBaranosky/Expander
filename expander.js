391ec56e3f3f88e680817a7a0078fec9017ba4db
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-19 14:25:12 -0400

    adds expansion/contraction effect to all four on the page

--- expander.js
+++ expander.js
@@ -1,6 +1,14 @@
 (function() {
-	function start() {
-		alert("JS doesn't do anything yet.");
-	}
-	Event.observe(window, 'load', start);
+    function start() {
+        function squareId(num) { return 'square' + num; }
+        function expanderId(num) { return 'expander' + num; }
+
+        $R('1', '4').each(function(num) {
+            Event.observe($(squareId(num)), 'click', function() {
+                Effect.toggle(expanderId(num), 'blind', { duration: 0.5 });
+            });
+        });
+    }
+
+    Event.observe(window, 'load', start);
 }());
