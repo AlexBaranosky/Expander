b254095b19d865ae53a52071cbea7ae42f10a0a8
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-19 19:51:53 -0400

    fixing spacing

--- expander.js
+++ expander.js
@@ -1,23 +1,23 @@
 (function() {
-    function addEvents() {		
-		function squareText(squareElement) {			
+    function addEvents() {
+        function squareText(squareElement) {
             return squareElement.innerText == '+' ? '-' : '+';
-		}
+        }
 
         function squareColor(squareElement) {
             return squareElement.getStyle('background-color') == 'white' ? 'red' : 'white';
         }
-		
+
         var widgets = $$('div.section').collect(function(section) {
             var square = Element.down(section, 'div.square');
             var expander = Element.down(section, 'div.expander');
             return { squareElement: square, expanderElement: expander};
-        });            
+        });
         widgets.each(function(widget) {
-			var square = widget.squareElement;
+            var square = widget.squareElement;
             Event.observe(square, 'click', function() {
-				square.innerText = squareText(square);	
-				square.setStyle({ backgroundColor: squareColor(square)});				
+                square.innerText = squareText(square);
+                square.setStyle({ backgroundColor: squareColor(square)});
                 Effect.toggle(widget.expanderElement, 'blind', { duration: 0.5 });
             });
         });
