4b07454adfdd125d1d1505d044d0d146d2ec4353
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-19 14:47:59 -0400

    trying to reformat code

--- expander.js
+++ expander.js
@@ -1,12 +1,12 @@
 (function() {
-    function addEvents() {	
-		var widgets = $$('div.section').collect(function(section) {
-			var square = Element.down(section, 'div.square');
-			var expander = Element.down(section, 'div.expander');
-			return { squareElement: square, 
-			         expanderElement: expander};
-		})
-	
+    function addEvents() {
+        var widgets = $$('div.section').collect(function(section) {
+            var square = Element.down(section, 'div.square');
+            var expander = Element.down(section, 'div.expander');
+            return { squareElement: square,
+                     expanderElement: expander};
+        });
+
         widgets.each(function(widget) {
             Event.observe(widget.squareElement, 'click', function() {
                 Effect.toggle(widget.expanderElement, 'blind', { duration: 0.5 });
