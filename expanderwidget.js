2fbc92ff397a650a526caf574d74e4a819e291cd
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-19 22:45:12 -0400

    made two functions locally scoped

--- expanderwidget.js
+++ expanderwidget.js
@@ -1,12 +1,12 @@
-function squareText(squareElement) {
-    return squareElement.innerText == '+' ? '-' : '+';
-}
-
-function squareColor(squareElement) {
-    return squareElement.getStyle('background-color') == 'white' ? 'red' : 'white';
-}
-
 function observeOnClick(expanderWidget) {
+    function squareText(squareElement) {
+        return squareElement.innerText == '+' ? '-' : '+';
+    }
+
+    function squareColor(squareElement) {
+        return squareElement.getStyle('background-color') == 'white' ? 'red' : 'white';
+    }
+
     var square = expanderWidget.squareElement;
     Event.observe(square, 'click', function() {
         square.innerText = squareText(square);
